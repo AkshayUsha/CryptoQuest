bb50a6dcf5e3f805ca6ba10c2e18d4463aee8ed8
“AkshayUsha” <akshaygupta.gupta9@gmail.comAkshayG@1392[200~git config --global user.email akshaygupta.gupta9@gmail.com>
2024-07-14 05:19:13 +0000

    Created Project Dir New Folder and files

--- obj/Debug/net8.0/CryptoQuest.AssemblyInfo.cs
+++ obj/Debug/net8.0/CryptoQuest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CryptoQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a67b9ec3229acb1454f1cf89948d8145e7f3ca8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0589d4e56d6ca749796156c50f7d3acb7843d8d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CryptoQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CryptoQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
